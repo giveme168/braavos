eafd243bb486fd85d373b8fe1fdef6d9e1185a27
qingfeng <qingfenghello@gmail.com>
2014-07-19 11:41:28 +0800

    第一個正式的測試，帶上了數據庫，接下來可以補充更多測試了

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,2 +1,15 @@
+from braavos.models.user import User, Team
+from braavos.config import DEFAULT_PASSWORD
+
+
 def test_hello():
     assert 1 == 1
+
+
+def test_create_user(session):
+    team1 = Team(name='testteam1')
+    user1 = User(name='testuser1', email='testuser1@inad.com',
+                 password=DEFAULT_PASSWORD, phone='111111', team=team1)
+    user1.add()
+    users = User.all()
+    assert len(users) == 1
