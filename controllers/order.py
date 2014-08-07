16080ae52712cf59cbe5927c77cf996f48bd0377
bingwen <bingwenst@foxmail.com>
2014-08-07 07:04:51 +0000

    使用Flask jsonify

--- controllers/order.py
+++ controllers/order.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 
 from flask import Blueprint, request, redirect, abort, url_for, g
-from flask import render_template as tpl, json
+from flask import render_template as tpl, json, jsonify
 
 from forms.order import OrderForm
 from forms.item import ItemForm
@@ -143,7 +143,7 @@ def schedules_post(order_id):
     status, msg = check_schedules_post(data)
     if not status:
         add_schedules(order, data)
-    return json.dumps({'status': status, 'msg': msg})
+    return jsonify({'status': status, 'msg': msg})
 
 
 def schdule_info(date, num):
@@ -165,7 +165,7 @@ def schedule_info():
     start_date = datetime.strptime(request.values.get('start'), '%Y-%m-%d').date()
     end_date = datetime.strptime(request.values.get('end'), '%Y-%m-%d').date()
     position = AdPosition.get(request.values.get('position'))
-    return json.dumps(get_schedule(position, start_date, end_date))
+    return jsonify(get_schedule(position, start_date, end_date))
 
 
 @order_bp.route('/position_list', methods=['GET'])
@@ -173,7 +173,7 @@ def position_list():
     #order = Order.get(request.values.get('order'))
     #sale_type = request.values.get('sale_type')
     #special_sale = request.values.get('special_sale')
-    return json.dumps([(p.id, p.display_name) for p in AdPosition.all()])
+    return jsonify([(p.id, p.display_name) for p in AdPosition.all()])
 
 
 @order_bp.route('/item/<item_id>', methods=["GET", "POST"])
