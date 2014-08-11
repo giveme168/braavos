2dc63e7ff2b3c38ec3cd5a3eb0219dab2f7d30f4
bingwen <bingwenst@foxmail.com>
2014-08-11 10:35:53 +0000

    增加fabfile.py

---
+++ tools/fabfile.py
@@ -0,0 +1,22 @@
+from fabric.api import *
+
+env.user = "inad"
+env.directory = "/home/inad/apps/braavos/"
+env.hosts = ['z.inad.com']
+
+
+def prepare_deploy():
+    local("echo ------------------------")
+    local("echo DEPLOYING Braavos TO PRODUCTION")
+    local("echo ------------------------")
+
+
+def restart_service():
+    run("sudo supervisorctl restart braavos")
+
+
+def deploy():
+    prepare_deploy()
+    with cd(env.directory):
+        run("git pull")
+    restart_service()
