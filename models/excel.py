1207f5327eb7d79d5e5c588a0f56a47bfc35953e
FlyingGoblin <cylily93@gmail.com>
2014-09-10 11:43:58 +0800

    修改pep8错误

--- models/excel.py
+++ models/excel.py
@@ -1,16 +1,17 @@
 # -*- coding: UTF-8 -*-
-from xlwt import (XFStyle, Font, Alignment,
-    Borders, Pattern, Workbook, Utils, Formula)
+from xlwt import (
+    XFStyle, Font, Alignment, Borders,
+    Pattern, Workbook, Utils, Formula)
 
 COLOUR_RED = 2
 COLOUR_LIGHT_GRAY = 22
 COLOUR_BLACK = 0
 FONT_SIZE_UNIT = 20
 
-EXCEL_DATE_TYPE_MERGE = 0
-EXCEL_DATE_TYPE_STR = 1
-EXCEL_DATE_TYPE_NUM = 2
-EXCEL_DATE_TYPE_FORMULA = 3
+EXCEL_DATA_TYPE_MERGE = 0
+EXCEL_DATA_TYPE_STR = 1
+EXCEL_DATA_TYPE_NUM = 2
+EXCEL_DATA_TYPE_FORMULA = 3
 
 
 class StyleFactory(object):
@@ -57,9 +58,9 @@ class StyleFactory(object):
 
 class ExcelCellItem(object):
     """docstring for ExcelCellItem"""
-    def __init__(self, type, date=" ", style=StyleFactory().style, merge_row=0, merge_col=0):
+    def __init__(self, type, data=" ", style=StyleFactory().style, merge_row=0, merge_col=0):
         self.type = type
-        self.date = date
+        self.data = data
         self.style = style
         self.merge_col = merge_col
         self.merge_row = merge_row
@@ -79,20 +80,20 @@ class Excel(object):
             self.sheet.row(row).height = self.row_height
             for col in range(0, len(excel_table[row])):
                 cell = excel_table[row][col]
-                if cell.type == EXCEL_DATE_TYPE_STR:
+                if cell.type == EXCEL_DATA_TYPE_STR:
                     self.sheet.write_merge(
-                        row, row + cell.merge_row, col, col + cell.merge_col, cell.date, cell.style)
-                    if str_len(cell.date) > 11:
-                        min_width = self.col_width * (str_len(cell.date) / 12 + 1)
+                        row, row + cell.merge_row, col, col + cell.merge_col, cell.data, cell.style)
+                    if str_len(cell.data) > 11:
+                        min_width = self.col_width * (str_len(cell.data) / 12 + 1)
                         self.sheet.col(col).width = (
                             self.sheet.col(col).width if self.sheet.col(col).width > min_width
                             else min_width)
-                if cell.type == EXCEL_DATE_TYPE_NUM:
+                if cell.type == EXCEL_DATA_TYPE_NUM:
                     self.sheet.write_merge(
-                        row, row + cell.merge_row, col, col + cell.merge_col, cell.date, cell.style)
-                if cell.type == EXCEL_DATE_TYPE_FORMULA:
+                        row, row + cell.merge_row, col, col + cell.merge_col, cell.data, cell.style)
+                if cell.type == EXCEL_DATA_TYPE_FORMULA:
                     self.sheet.write_merge(
-                        row, row + cell.merge_row, col, col + cell.merge_col, Formula(cell.date), cell.style)
+                        row, row + cell.merge_row, col, col + cell.merge_col, Formula(cell.data), cell.style)
         return self.xls
 
 
