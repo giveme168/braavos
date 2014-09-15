464cd0dc335c1c0e3383faa0947175e2f740096b
bingwen <bingwenst@foxmail.com>
2014-09-15 07:36:01 +0000

    使用Migrate

---
+++ migrations/versions/289dfb84c199_init.py
@@ -0,0 +1,22 @@
+"""init
+
+Revision ID: 289dfb84c199
+Revises: None
+Create Date: 2014-09-15 07:31:43.798991
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '289dfb84c199'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
