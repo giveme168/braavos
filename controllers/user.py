0e636a2f5290dd99f1f410d039646a883882b0d3
whuwb <skywind2006@gmail.com>
2014-09-29 19:27:19 +0800

    Issue 60: 只有管理员可见全部订单和全部广告素材

--- controllers/user.py
+++ controllers/user.py
@@ -26,7 +26,7 @@ def login():
             login_user(user)
             if user.check_password(DEFAULT_PASSWORD):
                 flash(u'您还在使用默认密码, 请及时<a href="%s">修改您的密码!</a>' % url_for('user.pwd_change'), 'danger')
-            return redirect(request.args.get("next", "/")
+            return redirect(request.args.get("next", "/"))
     return tpl('login.html', form=form)
 
 
