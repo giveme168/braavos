a830435e629663d0f69a9e48837bfa6b252a60e5
guoyu <guoyu@inad.com>
2015-07-01 17:10:16 +0800

    执行成本导出excel

--- controllers/data_query/finance.py
+++ controllers/data_query/finance.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request
 from flask import render_template as tpl
 
 from models.client_order import ClientOrderExecutiveReport
+from controllers.data_query.helpers.finance_helpers import write_order_excel
 
 data_query_finance_bp = Blueprint(
     'data_query_finance', __name__, template_folder='../../templates/data_query')
@@ -18,5 +19,8 @@ def order():
     now_month = datetime.datetime.strptime(year + '-' + month, '%Y-%m')
     client_orders = list(set([k.client_order for k in ClientOrderExecutiveReport.query.filter_by(
         month_day=now_month) if k.client_order.status == 1]))
+    if request.values.get('action', '') == 'download':
+        response = write_order_excel(client_orders, year, month)
+        return response
     return tpl('/data_query/finance/order.html', year=int(year),
                month=int(month), client_orders=client_orders)
