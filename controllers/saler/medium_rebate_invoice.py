3c92c7cc12475dbf94a60b14f87d6061d2fadc6a
Pandafisher <liukechun@gmail.com>
2015-07-03 19:36:18 +0800

    add new controller

---
+++ controllers/saler/medium_rebate_invoice.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint
+
+
+saler_medium_rebate_invoice_bp = Blueprint(
+    'saler_medium_rebate_invoice', __name__, template_folder='../../templates/saler')
