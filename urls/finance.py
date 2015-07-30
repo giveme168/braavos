ed9a7f3ba544964a7b909eca900890018f32773c
guoyu <guoyu@inad.com>
2015-07-30 15:20:26 +0800

    提交豆瓣订单回款功能

--- urls/finance.py
+++ urls/finance.py
@@ -4,6 +4,7 @@ from controllers.finance.medium_pay import finance_medium_pay_bp
 from controllers.finance.agent_pay import finance_agent_pay_bp
 from controllers.finance.client_order_back_money import finance_client_order_back_money_bp
 from controllers.finance.medium_rebate_invoice import finance_medium_rebate_invoice_bp
+from controllers.finance.douban_order.back_money import finance_douban_order_back_money_bp
 
 def finance_register_blueprint(app):
     app.register_blueprint(finance_invoice_bp, url_prefix='/finance/invoice')
@@ -12,3 +13,4 @@ def finance_register_blueprint(app):
     app.register_blueprint(finance_agent_pay_bp, url_prefix='/finance/agent_pay')
     app.register_blueprint(finance_client_order_back_money_bp, url_prefix='/finance/client_order_back_money')
     app.register_blueprint(finance_medium_rebate_invoice_bp, url_prefix='/finance/medium_rebate_invoice')
+    app.register_blueprint(finance_douban_order_back_money_bp, url_prefix='/finance/douban_order/back_money')
--- urls/saler.py
+++ urls/saler.py
@@ -3,6 +3,7 @@ from controllers.saler.medium_invoice import saler_medium_invoice_bp
 from controllers.saler.client_order_back_money import saler_client_order_back_money_bp
 from controllers.saler.agent_invoice import  saler_agent_invoice_bp
 from controllers.saler.medium_rebate_invoice import saler_medium_rebate_invoice_bp
+from controllers.saler.douban_order.back_money import saler_douban_order_back_money_bp
 
 def saler_register_blueprint(app):
     app.register_blueprint(saler_invoice_bp, url_prefix='/saler/invoice')
@@ -10,3 +11,4 @@ def saler_register_blueprint(app):
     app.register_blueprint(saler_client_order_back_money_bp, url_prefix='/saler/client_order_back_money')
     app.register_blueprint(saler_agent_invoice_bp, url_prefix='/saler/agent_invoice')
     app.register_blueprint(saler_medium_rebate_invoice_bp, url_prefix='/saler/medium_rebate_invoice')
+    app.register_blueprint(saler_douban_order_back_money_bp, url_prefix='/saler/douban_order/back_money')
