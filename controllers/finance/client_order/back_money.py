cc6e944c41fd0e9982b7124d023feb25cb3dfb16
guoyu <guoyu@inad.com>
2015-08-31 17:36:49 +0800

    添加回款提醒

--- controllers/finance/client_order/back_money.py
+++ controllers/finance/client_order/back_money.py
@@ -2,11 +2,12 @@
 import datetime
 
 from flask import request, Blueprint, abort, flash, g, redirect, url_for
-from flask import render_template as tpl
+from flask import current_app, render_template as tpl
 
 from models.user import TEAM_LOCATION_CN
 from libs.paginator import Paginator
 from models.client_order import ClientOrder, BackMoney, BackInvoiceRebate, CONTRACT_STATUS_CN
+from libs.signals import back_money_apply_signal
 
 finance_client_order_back_money_bp = Blueprint(
     'finance_client_order_back_money', __name__, template_folder='../../templates/finance/client_order')
@@ -78,6 +79,16 @@ def back_money(order_id):
                 order.save()
                 flash(u'完成所有回款!', 'success')
                 order.add_comment(g.user, u"完成所有回款", msg_channel=4)
+                apply_context = {
+                    'order': order,
+                    'num': 0,
+                    'type': 'end',
+                }
+                back_money_apply_signal.send(
+                    current_app._get_current_object(), apply_context=apply_context)
+                flash(u'更新了回款状态，回款已完成!', 'success')
+                order.add_comment(
+                    g.user, u"更新了回款状态，回款已完成;", msg_channel=4)
         else:
             bm = BackMoney.add(
                 client_order=order,
@@ -85,6 +96,13 @@ def back_money(order_id):
                 back_time=back_time,
                 create_time=datetime.date.today().strftime('%Y-%m-%d'))
             bm.save()
+            apply_context = {
+                'order': order,
+                'num': money,
+                'type': 'money',
+            }
+            back_money_apply_signal.send(
+                current_app._get_current_object(), apply_context=apply_context)
             flash(u'回款信息保存成功!', 'success')
             order.add_comment(
                 g.user, u"更新了回款信息，回款金额: %s; 回款时间: %s;" % (money, back_time), msg_channel=4)
@@ -114,6 +132,14 @@ def back_invoice(order_id):
         flash(u'返点发票信息保存成功!', 'success')
         order.add_comment(
             g.user, u"更新了返点发票信息，发票金额: %s; 发票时间: %s; 发票号: %s;" % (money, back_time, num), msg_channel=4)
+
+        apply_context = {
+            'order': order,
+            'num': money,
+            'type': 'invoice',
+        }
+        back_money_apply_signal.send(
+            current_app._get_current_object(), apply_context=apply_context)
         return redirect(url_for("finance_client_order_back_money.back_money", order_id=order.id))
     return tpl('/finance/client_order/back_money/info.html', order=order)
 
