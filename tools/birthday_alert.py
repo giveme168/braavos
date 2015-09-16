b675790f7c411d59c2d23c25cf1917875f371af6
guoyu <guoyu@inad.com>
2015-09-16 15:34:17 +0800

    添加媒体订单发票与款项报表

--- tools/birthday_alert.py
+++ tools/birthday_alert.py
@@ -1,28 +1,43 @@
 # encoding: utf-8
 import os
+import datetime
 import sys
-sys.path.append('/Users/guoyu1/workspace/inad/braavos') 
-#sys.path.append('/home/inad/apps/braavos/releases/current') 
+#sys.path.append('/Users/guoyu1/workspace/inad/braavos') 
+sys.path.append('/home/inad/apps/braavos/releases/current') 
+
 from app import app
+
 from models.user import User
 from libs.mail import send_html_mail
 
 
 
 
-def get_all_today_is_birthday():
+def get_all_today_is_birthday(name, email):
     text = u"""
-    <div class="container-fluid" tabindex="-1" style="background-image:url(http://z.inad.com/static/imgs/brithday.jpg);background-repeat: no-repeat; background-position: center 0;min-height:660px;width:700px;position: relative;">
-      <div style="position:absolute;left:27%%;top:22%%;color:#FF44AA; font-family:Microsoft YaHei;width:370px;font-size:20px;">
-        <h3 >亲爱的 %s ：<br/>&nbsp;&nbsp;&nbsp;&nbsp;感谢一直以来在致趣的付出以及努力，今天是你的生日，在这个格外重要的日子代表致趣小伙伴们<br/>&nbsp;&nbsp;&nbsp;&nbsp;祝你生日快乐，你最珍贵！</h3>
+    <div class="container-fluid" tabindex="-1" style="background-image:url(http://z.inad.com/static/imgs/brithday_1.jpg);background-repeat: no-repeat; background-position: center 0;min-height:660px;width:700px;position: relative;">
+      <div style="position:absolute;left:38%%;top:18%%;color:#FF44AA; font-family:Microsoft YaHei;width:360px;font-size:20px;">
+        <h3 >%s ：</h3>
       <div>
     </div>
 
     <br/><br/>
-    """%(u'郭钰')
-    to_emails = ['guoyu@inad.com']
-    send_html_mail(u'生日快乐！', recipients=to_emails, body=text)
-    
+    """%(name)
+    to_emails = [email]
+    try:
+        send_html_mail(u'生日快乐！', recipients=to_emails, body=text)
+    except:
+        pass
 
 if __name__ == '__main__':
-    get_all_today_is_birthday()
+    now_date = datetime.datetime.now().strftime('%m%d')
+    users = User.all_active()
+    for k in users:
+        try:
+            birthday = k.birthday.strftime('%m%d')
+            ini_birthday = k.birthday.strftime('%Y%m%d')
+            if ini_birthday != '19700101':
+                if birthday == now_date:
+                    get_all_today_is_birthday(k.name, k.email)
+        except:
+            pass
