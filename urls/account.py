b4929f0c3ebe2243bfc6c491547b5e14c8388e65
guoyu <guoyu@inad.com>
2015-11-10 15:28:44 +0800

    修复搜索业务邮件问题

--- urls/account.py
+++ urls/account.py
@@ -3,6 +3,7 @@ from controllers.account.leave import account_leave_bp
 from controllers.account.out import account_out_bp
 from controllers.account.performance import account_performance_bp
 from controllers.account.commission import account_commission_bp
+from controllers.account.data import account_data_bp
 
 def account_register_blueprint(app):
     app.register_blueprint(account_kpi_bp, url_prefix='/account/kpi')
@@ -10,3 +11,4 @@ def account_register_blueprint(app):
     app.register_blueprint(account_out_bp, url_prefix='/account/out')
     app.register_blueprint(account_performance_bp, url_prefix='/account/performance')
     app.register_blueprint(account_commission_bp, url_prefix='/account/commission')
+    app.register_blueprint(account_data_bp, url_prefix='/account/data')
