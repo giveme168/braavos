ba5847a009a803e9d8464c3bcb7b9c6081dd2e8d
guoyu <guoyu@inad.com>
2016-04-01 10:21:10 +0800

    添加年会结果

--- controllers/other.py
+++ controllers/other.py
@@ -6,6 +6,19 @@ from models.other import NianHui
 other_bp = Blueprint('other', __name__, template_folder='../templates/other')
 
 
+JM = {1: u'《try everthing》',
+      2: u'《歌曲串烧》',
+      3: u'《致趣联播》',
+      4: u'《我也是歌手》',
+      5: u'《脱口秀》',
+      6: u'《蒙面歌王》',
+      7: u'《INAD加油》',
+      8: u'《uptown funk》',
+      9: u'《极限挑战》',
+      10: u'《台前幕后》',
+      11: u'《狐狸说嘛呢》'}
+
+
 @other_bp.route('/nianhui', methods=['GET', 'POST'])
 def nianhui():
     if request.method == 'POST':
@@ -16,4 +29,18 @@ def nianhui():
                     create_time=datetime.datetime.now(),
                     ids=ids)
         return jsonify({'status': 0, 'msg': '1123'})
-    return tpl('/other/nhs.html')
+    ids = '|'.join([k.ids for k in NianHui.all()])
+    jm_count = {}
+    total_count = 0
+    for k, v in JM.items():
+        jm_count[k] = 0
+    for k in ids.split('|'):
+        total_count += 1
+        if int(k) in jm_count:
+            jm_count[int(k)] += 1
+    jm_count = sorted(jm_count.iteritems(), key=lambda x: x[1])
+    jm_count.reverse()
+    res = []
+    for k in jm_count:
+        res.append({'name': JM[k[0]], 'count': k[1], 'percent': float(k[1])/total_count*100})
+    return tpl('/other/nhs.html', res=res, total_count=total_count)
