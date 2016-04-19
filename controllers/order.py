15dafd049e5330ccd20d362ab5a9aa59b4b53e3e
guoyu <guoyu@inad.com>
2016-04-19 11:40:22 +0800

    合同管理员打开单笔返点权限

--- controllers/order.py
+++ controllers/order.py
@@ -339,7 +339,7 @@ def order_info(order_id, tab_id=1):
                     order.contract_type = client_form.contract_type.data
                     order.resource_type = client_form.resource_type.data
                     order.sale_type = client_form.sale_type.data
-                    if g.user.is_super_leader():
+                    if g.user.is_super_leader() or g.user.is_contract():
                         order.replace_sales = User.gets(
                             replace_saler_form.replace_salers.data)
                         order.self_agent_rebate = str(self_rebate) + '-' + str(self_rabate_value)
@@ -1368,7 +1368,7 @@ def douban_order_info(order_id):
                     order.contract_type = form.contract_type.data
                     order.resource_type = form.resource_type.data
                     order.sale_type = form.sale_type.data
-                    if g.user.is_super_admin():
+                    if g.user.is_super_admin() or g.user.is_contract():
                         order.replace_sales = User.gets(
                             replace_saler_form.replace_salers.data)
                         order.self_agent_rebate = str(self_rebate) + '-' + str(self_rabate_value)
